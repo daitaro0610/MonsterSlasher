22de751babaf0dd64d948026fc1e50ea3a955449
daitaro0610 <114137856+daitaro0610@users.noreply.github.com>
2024-02-03 13:05:42 +0900

    Add files via upload
    <add>ManagerScript

---
+++ Script/Manager/Script/ManagerManagement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerManagement : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] m_Managers;
+
+    private void Awake()
+    {
+        foreach(GameObject manager in m_Managers)
+        {
+            Instantiate(manager);
+        }
+        Destroy(gameObject);
+    }
+}
